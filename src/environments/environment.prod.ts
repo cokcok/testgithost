b2e56597f8182a6014a3df7670b2319e12e97a94
cokcok <wattanai@rubber.mail.go.th>
2021-01-13 18:00:01 +0700

    push notification

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDcDoe-Xd-G_JYL6JO9u7Ra7vraAOxfmHg",
+    authDomain: "duangdeewin.firebaseapp.com",
+    projectId: "duangdeewin",
+    storageBucket: "duangdeewin.appspot.com",
+    messagingSenderId: "476653985053",
+    appId: "1:476653985053:web:efe8694cf26c892a6bd5f4",
+    measurementId: "G-D7073S0JTS",
+    vapidKey: 'BIGDHEwddFF0ypuKGidQ8OS-b3LeiAMjfkK_X5ljbH_Dt7fld6uct2QblCqciBfTVyF4B33inCnhvlUiu0fCyJY'
+  }
 };
