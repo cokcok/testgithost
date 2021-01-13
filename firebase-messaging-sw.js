1deb637ff34ead50e7f099de09824c7baddc4291
cokcok <wattanai@rubber.mail.go.th>
2021-01-13 18:04:31 +0700

    Auto-generated commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/5.7.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.7.3/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '476653985053'
+});
+
+const messaging = firebase.messaging();
